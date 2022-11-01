317add4c6bfe0c874519c41956ac8b73f344c2d5
nestor <robinzonejob@gmail.com>
2022-11-01 23:40:56 +0300

    * first commit

---
+++ RoleD/RoleD/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Привет, Путник!</h1>
+            <p>Совсем скоро здесь появится что-то очень интересное</p>
+      </div>
+    );
+  }
+}
